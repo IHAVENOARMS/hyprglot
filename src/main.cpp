3ea798c0611abe5ee761230459aac8bcb8c0a6a3
Yousef Gamal <Yousffgamal123@gmail.com>
2025-03-10 08:20:51 +0200

    chore: testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "./globals.hpp"
+#include <hyprland/src/desktop/Window.hpp>
 #include <hyprland/src/SharedDefs.hpp>
 #include <hyprland/src/plugins/PluginAPI.hpp>
 
@@ -8,13 +9,13 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
 
     const std::string HASH = __hyprland_api_get_hash();
     if (HASH != GIT_COMMIT_HASH) {
-        HyprlandAPI::addNotificationV2(PHANDLE,
-                                       {{"text", "[hyprglot] Failure in Initialization: Version mismatch (headers version is not equal to running hyprland version)"},
-                                        {"time", 5000},
-                                        {"color", CHyprColor{1.0, 0.2, 0.2, 1.0}},
-                                        {"icon", ICON_ERROR}});
+        HyprlandAPI::addNotification(PHANDLE, "[hyprglot] Failure in Initialization: Version mismatch (headers version is not equal to running hyprland version)",
+                                     CHyprColor{1.0, 0.2, 0.2, 1.0}, 5000);
+
         throw std::runtime_error("[hyprglot] Version mismatch");
     }
 
-    HyprlandAPI::addNotificationV2(PHANDLE, {{"text", "[hyprglot] Hello!"}, {"time", 5000}, {"color", CHyprColor{1.0, 0.2, 0.2, 1.0}}, {"icon", ICON_INFO}});
+    HyprlandAPI::addNotification(PHANDLE, "[hyprglot] Initialized successfully!", CHyprColor{0.2, 1.0, 0.2, 1.0}, 5000);
+
+    return {"hyprglot", "A plugin that facilitates managing multiple keyboard languages in hyprland", "Jimmy (Yousef)", "1.0"};
 }
